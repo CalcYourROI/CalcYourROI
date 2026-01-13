f8ce71de906a069d9192d8734dfcda9d6a6f6d97
CalcYourROI <calcyourroi@gmail.com>
2026-01-13 13:08:30 -0600

    Add homepage

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main style={{ padding: 40 }}>
+      <h1>CalcYourROI™</h1>
+      <p>Rental Property ROI Calculator</p>
+      <a href="/calculator">Go to Calculator</a>
+    </main>
+  )
+}
